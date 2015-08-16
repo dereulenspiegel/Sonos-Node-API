7599d91d1cb8eba74fbb40b3057a986ec84b071a
Till Klocke <till.klocke@gmail.com>
2015-08-16 16:01:07 +0200

    Initial commit, basic structure should be there

---
+++ test.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var sonosService = require("./lib/sonos/sonos-service.js");
+
+var mediaItems = [
+  {
+    id: "1",
+    title: "test-items",
+    mimeType: "mpeg3",
+    itemType: "stream",
+    streamMetadata: {
+      currentShowId: "show-id",
+      currentShow: "currentshow",
+      secondsRemaining: 0,
+      secondsToNextShow: 0,
+      hasOutOfBandMetadata: false,
+      description: "cool uberstream",
+      isEphemeral: true
+    }
+  },
+
+  {
+    id: "2",
+    title: "another est-items",
+    mimeType: "mpeg3",
+    itemType: "stream",
+    streamMetadata: {
+      currentShowId: "show-id2",
+      currentShow: "nextshow",
+      secondsRemaining: 0,
+      secondsToNextShow: 0,
+      hasOutOfBandMetadata: false,
+      description: "cool uberstream2",
+      isEphemeral: true
+    }
+  }
+];
+
+var sourceManager = {
+  getLastUpdate: function() {
+    return 1;
+  },
+
+  getItemCount: function(id){
+    return mediaItems.length;
+  },
+
+  getItems: function(id, index, count, recursive) {
+    if(id === "root" && index===0 ){
+      return mediaItems;
+    } else {
+      console.log("Inavlid id or range id %s, index %d, count %d",id, index, count);
+    }
+  },
+
+  getItem: function(id) {
+    for(var x in mediaItems){
+      var item = mediaItems[x];
+      if(item.id === id){
+        return item;
+      }
+    }
+    return null;
+  }
+};
+
+console.log("Setting storage bacend");
+
+sonosService.setStorageManager(sourceManager);
+console.log("Starting sonos service");
+sonosService.start();
+
